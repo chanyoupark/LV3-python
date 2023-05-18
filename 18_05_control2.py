fc0696e647faa2d543f846604d90db752374bbaa
Aiden Park <1559pac@gmail.com>
2023-05-18 17:57:44 +1000

    V2

---
+++ 18_05_control2.py
@@ -0,0 +1,8 @@
+a = 4
+b = 7
+if a > 0 and b > 0:
+    print('Both are positive numbers')
+if a%2 or b%2:
+    print('Either of one is even')
+if a > 0 and not b < 0:
+    print("Both are positive")
